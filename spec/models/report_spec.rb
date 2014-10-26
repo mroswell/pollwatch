e24b991c283227346b2bd52923207764e3a1ac09
Volkan Unsal <spocksplanet@gmail.com>
2014-10-25 20:26:22 -0400

    add test for the cope

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Report, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context ".current_election_year" do
+    it "should return reports from the current election year" do
+      current_year = Time.now.year
+      create_list(:report, 2, election_year: 2013)
+      curYearReports = create_list(:report, 2, election_year: current_year)
+      expect(Report.current_election_year.all).to eq curYearReports
+    end
+  end
+
+
 end
