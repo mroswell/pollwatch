14dcd9baff0b041af75131dc7da0cd0d617d189c
Volkan Unsal <spocksplanet@gmail.com>
2014-10-27 10:30:48 -0400

    modify the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 require "domain_constraint"
-  
+
 Rails.application.routes.draw do
   get '/reporting' => 'reporting#home'
   resources :reports, except: [:delete, :edit, :update] do
@@ -15,10 +15,14 @@ Rails.application.routes.draw do
 
 
   if Rails.env.production?
+    # FIXME: error
+    # ArgumentError: Invalid route name, already in use: 'root'
+    #        You may have defined two routes with the same name using the `:as` option, or you may be overriding a route already defined by a resource with the same naming. For the latter, you can restrict the routes created with `resources` as explained here:
+    #        http://guides.rubyonrails.org/routing.html#restricting-the-routes-created
     constraints DomainConstraint.new('pollwatch.us') do
       root to: 'reporting#home'
     end
   end
+  root to: 'pages#home'
 
-	root to: 'pages#home'
 end
